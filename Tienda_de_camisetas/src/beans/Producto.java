2fdd0b77bdd4e56a34de773dbec503f364501f3d
unknown <p.torralba.moron@gmail.com>
2017-03-15 12:29:25 +0100

    java con cosas

---
+++ Tienda_de_camisetas/src/beans/Producto.java
@@ -0,0 +1,37 @@
+package beans;
+
+public class Producto {
+	private int idproducto;
+	private String descripcion;
+	private int categoria;
+	private float precio;
+	
+	
+	
+	public int getIdproducto() {
+		return idproducto;
+	}
+	public void setIdproducto(int idproducto) {
+		this.idproducto = idproducto;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public int getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(int categoria) {
+		this.categoria = categoria;
+	}
+	public float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	
+	
+}
