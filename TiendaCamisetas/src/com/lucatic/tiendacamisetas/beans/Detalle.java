30f04334e3ccceb42b9aad57d36585f643b540f1
tombisky <tombysky@gmail.com>
2017-03-17 10:23:48 +0100

    modificado clase factura y detalle

--- TiendaCamisetas/src/com/lucatic/tiendacamisetas/beans/Detalle.java
+++ TiendaCamisetas/src/com/lucatic/tiendacamisetas/beans/Detalle.java
@@ -3,53 +3,64 @@ package com.lucatic.tiendacamisetas.beans;
 public class Detalle {
 
 	private int idDetalle;
-	private int idProducto;
+	private Producto producto;
 	private int cantidad;
 	private float precio;
-	
-	public Detalle(){
-		
+
+	public Detalle() {
+
 	}
-	public Detalle(int idDetalle, int idProducto, int cantidad, float precio){
-		this.idDetalle=idDetalle;
-		this.idProducto=idProducto;
-		this.cantidad=cantidad;
-		this.precio=precio;
-		}
+
+	public Detalle(int idDetalle, Producto producto, int cantidad, float precio) {
+		this.idDetalle = idDetalle;
+		this.producto = producto;
+		this.cantidad = cantidad;
+		this.precio = precio;
+	}
+
 	public int getIdDetalle() {
 		return idDetalle;
 	}
+
 	public void setIdDetalle(int idDetalle) {
 		this.idDetalle = idDetalle;
 	}
-	public int getIdProducto() {
-		return idProducto;
+
+	public Producto getProducto() {
+		return producto;
 	}
-	public void setIdProducto(int idProducto) {
-		this.idProducto = idProducto;
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
 	}
+
 	public int getCantidad() {
 		return cantidad;
 	}
+
 	public void setCantidad(int cantidad) {
 		this.cantidad = cantidad;
 	}
+
 	public float getPrecio() {
 		return precio;
 	}
+
 	public void setPrecio(float precio) {
 		this.precio = precio;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + cantidad;
 		result = prime * result + idDetalle;
-		result = prime * result + idProducto;
 		result = prime * result + Float.floatToIntBits(precio);
+		result = prime * result + ((producto == null) ? 0 : producto.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -63,15 +74,19 @@ public class Detalle {
 			return false;
 		if (idDetalle != other.idDetalle)
 			return false;
-		if (idProducto != other.idProducto)
-			return false;
 		if (Float.floatToIntBits(precio) != Float.floatToIntBits(other.precio))
+			return false;
+		if (producto == null) {
+			if (other.producto != null)
+				return false;
+		} else if (!producto.equals(other.producto))
 			return false;
 		return true;
 	}
+
 	@Override
 	public String toString() {
-		return "Detalle [idDetalle=" + idDetalle + ", idProducto=" + idProducto + ", cantidad=" + cantidad + ", precio="
+		return "Detalle [idDetalle=" + idDetalle + ", producto=" + producto + ", cantidad=" + cantidad + ", precio="
 				+ precio + "]";
 	}
 }
--- TiendaCamisetas/src/com/lucatic/tiendacamisetas/beans/Factura.java
+++ TiendaCamisetas/src/com/lucatic/tiendacamisetas/beans/Factura.java
@@ -6,62 +6,88 @@ import java.util.List;
 import com.lucatic.tiendacamisetas.model.TipoPago;
 
 public class Factura {
-	
-	private List <Detalle> lista= new ArrayList<Detalle>();
+
+	// Creamos una lista de detalles, ya que factura se compone principalmente
+	// de detalles
+	private List<Detalle> lista = new ArrayList<Detalle>();
+	// tenemos el id del cliente que realiza la compra y genera la factura
 	private int idCliente;
+	// indicamos el tipo de pago (en este caso pertenece a un enum)
 	private TipoPago TipoPago;
+	// indicamos la fecha en la que se realiza la factura
 	private Date fecha;
+	// indicamos el precio total de la factura (con el metodo que tenemos mas
+	// abajo para ir sumando el precio de todos los detalles)
 	private float precioTotal;
-	
-	public Factura(){
-		
+
+	// constructor basico de factura
+	public Factura() {
+
 	}
-	public Factura(List <Detalle> detalles, int idCliente, TipoPago tipoPago, Date fecha){
-		this.lista=detalles;
-		this.idCliente=idCliente;
-		this.TipoPago=tipoPago;
-		this.fecha=fecha;
-		
-		this.precioTotal=calcularTotal(detalles);
+
+	// constructor completo de factura en el cual asignamos los valores
+	public Factura(List<Detalle> detalles, int idCliente, TipoPago tipoPago, Date fecha) {
+		this.lista = detalles;
+		this.idCliente = idCliente;
+		this.TipoPago = tipoPago;
+		this.fecha = fecha;
+		// para asignar el precio total llamamos a nuestro metodo para que lo
+		// calcule y nos devuelva el total
+		this.precioTotal = calcularTotal(detalles);
+	}
+
+	public float calcularTotal(List<Detalle> detalles) {
+		float total = 0;
+		// miramos la lista de 1 en 1 y vamos sumando los precios en nuestro
+		// total el cual está inicializado a 0
+		for (Detalle a : detalles) {
+			total = +a.getPrecio();
 		}
-	public float calcularTotal(List <Detalle> detalles){
-		float total=0;
-		for (Detalle a: detalles){
-			total=+a.getPrecio();
-		}
-		
+		// devolvemos el valor de total
 		return total;
 	}
+
+	// sets and gets de la clase factura
 	public List<Detalle> getLista() {
 		return lista;
 	}
+
 	public void setLista(List<Detalle> lista) {
 		this.lista = lista;
 	}
+
 	public int getIdCliente() {
 		return idCliente;
 	}
+
 	public void setIdCliente(int idCliente) {
 		this.idCliente = idCliente;
 	}
+
 	public TipoPago getTipoPago() {
 		return TipoPago;
 	}
+
 	public void setTipoPago(TipoPago tipoPago) {
 		TipoPago = tipoPago;
 	}
+
 	public Date getFecha() {
 		return fecha;
 	}
+
 	public void setFecha(Date fecha) {
 		this.fecha = fecha;
 	}
+
 	public float getPrecioTotal() {
 		return precioTotal;
 	}
+
 	public void setPrecioTotal(float precioTotal) {
 		this.precioTotal = precioTotal;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -73,6 +99,7 @@ public class Factura {
 		result = prime * result + Float.floatToIntBits(precioTotal);
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -100,10 +127,26 @@ public class Factura {
 			return false;
 		return true;
 	}
+
+	/*
+	 * metodo mostrar lista para usarlo en nuestro toString y que nos muestre
+	 * todos los productos de todos los detalles cargandolos en un StringBuffer y devolviendolo
+	 */
+	public String listadoDetalles() {
+		StringBuffer cadena = new StringBuffer();
+		for (Detalle d : this.lista) {
+			System.out.println("Descripcion producto: " + d.getProducto().getDescripcion() + " Id producto: "
+					+ d.getProducto().getIdProducto()+d.getCantidad());
+		}
+		return cadena.toString();
+	}
+	/*
+	 * Mostramos por panntalla los datos de factura utilizando nunestro metodo listadoDetalles para mostrar todos los detalles
+	 */
 	@Override
 	public String toString() {
-		return "Factura [lista=" + lista + ", idCliente=" + idCliente + ", TipoPago=" + TipoPago + ", fecha=" + fecha
-				+ ", precioTotal=" + precioTotal + "]";
+		return "Factura : " + listadoDetalles() + "idCliente=" + idCliente + ", TipoPago=" + TipoPago + ", fecha="
+				+ fecha + ", precioTotal=" + precioTotal;
 	}
-	
+
 }
