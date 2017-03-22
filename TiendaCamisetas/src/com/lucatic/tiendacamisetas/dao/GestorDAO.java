fd35f54778392801f40eac0094c5c922a9fa529d
CarlosCamposF <carloscf1991@gmail.com>
2017-03-22 15:53:26 +0100

    Actualizacion GestorDAO

--- TiendaCamisetas/src/com/lucatic/tiendacamisetas/dao/GestorDAO.java
+++ TiendaCamisetas/src/com/lucatic/tiendacamisetas/dao/GestorDAO.java
@@ -2,7 +2,7 @@ package com.lucatic.tiendacamisetas.dao;
 
 public interface GestorDAO<T> {
 	public void addItem(T item) throws DAOException;
-	public void removeItem(T item) throws DAOException;
+	public void removeItem(int item) throws DAOException;
 	public void updateItem(T item) throws DAOException;
-	public T getItem(T item) throws DAOException;
+	public T getItem() throws DAOException;
 }
