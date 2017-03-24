a90aa19f8f2d70a58046394b0ad42b073661fa6e
CarlosCamposF <carloscf1991@gmail.com>
2017-03-24 12:54:21 +0100

    Modificacion DAO de producto y test para producto

--- TiendaCamisetas/src/com/lucatic/tiendacamisetas/dao/ProductoDAO.java
+++ TiendaCamisetas/src/com/lucatic/tiendacamisetas/dao/ProductoDAO.java
@@ -1,14 +1,12 @@
 package com.lucatic.tiendacamisetas.dao;
 
 import java.util.ArrayList;
-
-import com.lucatic.tiendacamisetas.beans.Camiseta;
 import com.lucatic.tiendacamisetas.beans.Producto;
 
 public interface ProductoDAO extends GestorDAO<Producto>{
-	 public Producto findById(int id) throws DAOException;
-	 public ArrayList<Producto> findByColor(String color) throws DAOException;
-	 public ArrayList<Producto> findByTalla(String talla) throws DAOException;
-	 public void addCamiseta(Camiseta c) throws DAOException;
+	 public ArrayList<Producto> findCamisetaByColor(int color) throws DAOException;
+	 public ArrayList<Producto> findCamisetaByTalla(int talla) throws DAOException;
+	 public void addCamiseta(Producto c) throws DAOException;
 	 public ArrayList<Producto> AllCamiseta() throws DAOException;
+	 public void updateCamiseta(Producto item) throws DAOException;
 }
